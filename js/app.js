9c09b42589017fcce93742ddc9278550d97928ba
javlona <joeclaap@gmail.com>
2021-11-29 09:26:32 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const API_KEY = '5b1b515986ab2e1bc528fe6b762fd9a9'
+const url = ''
+const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+const nowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+const trending = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+
+
+
+
+
+// menu toggler
 const navLinks = document.querySelector('.links')
 const menuToggle = document.querySelector('#menu')
 
