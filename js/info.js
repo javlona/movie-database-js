ec65f490f30db7183e3349d12e3a434702d4041b
Javlon <59303287+javlona@users.noreply.github.com>
2021-12-07 18:22:02 -0600

    Update info.js

--- js/info.js
+++ js/info.js
@@ -1,16 +1,22 @@
 import * as constants from './constants.js'
-import {} from './utilities.js'
+import {creditsUrl, Storage} from './utilities.js'
 
 
 
+// get movie id from local Storage
+let movId = Storage.get('movie')
 
 
 // fetch movie credits for a particular movie
 async function fetchCredits() {
 
     try {
-        let response = await fetch(constants.creditsUrl())
+        let response = await fetch(creditsUrl(moivId))
         let credits = await response.json()
-
+        console.log(credits)
+        
+    } catch (error) {
+        console.log(error)
     }
+
 }
