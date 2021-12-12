406b858eb404230f4dc6e2ae42682a6e4e09a3a5
Javlon <59303287+javlona@users.noreply.github.com>
2021-12-12 14:52:58 -0600

    load button added

--- js/movies.js
+++ js/movies.js
@@ -4,15 +4,15 @@ import {popularUrl, movieCard, goToInfo } from './utilities.js'
 let page = 1;
 
 // fetch popular into cards
-async function fetchPopular() {
+async function fetchPopular(mediaType, name, date) {
 
     try {
-        let response = await fetch(popularUrl("movie",constants.API_KEY, page))
+        let response = await fetch(popularUrl(mediaType, constants.API_KEY, page))
         let popularData = await response.json()
-
+        console.log(popularData)
         popularData.results.forEach((item) => {
 
-            let card = movieCard(item.poster_path, item.title, item.release_date, item.vote_average, item.id, "card-tall")
+            let card = movieCard(item.poster_path, item[name], item[date], item.vote_average, item.id, "card-tall")
             constants.popularResults.innerHTML += card
         })
 
@@ -22,8 +22,25 @@ async function fetchPopular() {
 
 }
 
+// categories side menu movies
+constants.searchMoviesBtn.addEventListener('click', () => {
+    constants.popularResults.innerHTML = ""
+    fetchPopular("movie", "title", "release_date")
+})
+
+// categories side menu TV Shows
+constants.searchShowsBtn.addEventListener('click', () => {
+    constants.popularResults.innerHTML = ""
+    fetchPopular("tv", "name", "first_air_date")
+})
+
+// load more
+function loadMore() {
+    
+}
+
 window.addEventListener('load', () => {
-    fetchPopular()
+    fetchPopular("movie", "title", "release_date" )
 })
 
 window.goToInfo = goToInfo  
