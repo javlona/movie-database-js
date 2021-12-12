bc64ca57d5734c4be25176f682f4a0799fa815ad
Javlon <59303287+javlona@users.noreply.github.com>
2021-12-12 16:55:26 -0600

    Update movies.js

--- js/movies.js
+++ js/movies.js
@@ -2,6 +2,7 @@ import * as constants from './constants.js'
 import {popularUrl, movieCard, goToInfo } from './utilities.js'
 
 let page = 1;
+let mediaType = "movie";
 
 // fetch popular into cards
 async function fetchPopular(mediaType, name, date) {
@@ -9,9 +10,8 @@ async function fetchPopular(mediaType, name, date) {
     try {
         let response = await fetch(popularUrl(mediaType, constants.API_KEY, page))
         let popularData = await response.json()
-        console.log(popularData)
-        popularData.results.forEach((item) => {
 
+        popularData.results.forEach((item) => {
             let card = movieCard(item.poster_path, item[name], item[date], item.vote_average, item.id, "card-tall")
             constants.popularResults.innerHTML += card
         })
@@ -25,20 +25,34 @@ async function fetchPopular(mediaType, name, date) {
 // categories side menu movies
 constants.searchMoviesBtn.addEventListener('click', () => {
     constants.popularResults.innerHTML = ""
-    fetchPopular("movie", "title", "release_date")
+    mediaType = "movie"
+    fetchPopular(mediaType, "title", "release_date")
 })
 
 // categories side menu TV Shows
 constants.searchShowsBtn.addEventListener('click', () => {
     constants.popularResults.innerHTML = ""
-    fetchPopular("tv", "name", "first_air_date")
+    page = 1;
+    mediaType = "tv"
+    fetchPopular(mediaType, "name", "first_air_date")
 })
 
 // load more
 function loadMore() {
-    
+    if(mediaType === "movie") {
+        page++
+        fetchPopular(mediaType, "title", "release_date")
+    } else {
+        mediaType = "tv"
+        page++
+        fetchPopular(mediaType, "name", "first_air_date")
+    }
 }
 
+// run load more function
+document.querySelector('#loadMoreBtn').addEventListener('click', loadMore)
+
+// load popular movies on load
 window.addEventListener('load', () => {
     fetchPopular("movie", "title", "release_date" )
 })
